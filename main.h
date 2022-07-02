25f221d01f72ed93b37fe92b1b70bdc5e22341fa
EthanWakeford <ethanwakeford@gmail.com>
2022-07-01 20:10:03 -0700

    updated for linter, as well as updated to pass count argument to prints functions to check for errors

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-int (*get_func(char conv_spec))(va_list);
+int (*get_func(char conv_spec))(va_list, int);
 int print_char(char c);
 int print_str(char *c);
 int stringlength(char *str);
@@ -15,10 +15,16 @@ int print_dec(int c);
 int int_len(int c);
 int _putchar(char c);
 
+/**
+*struct type - matches format specifier and function pointer
+*@form_match: format specifier to match
+*@func: function to run
+*/
+
 typedef struct type
 {
-    char form_match;
-    int (*func)(va_list);
+	char form_match;
+	int (*func)(va_list, int);
 } type_t;
 
 #endif
--- printf.c
+++ printf.c
@@ -1,28 +1,41 @@
 #include "main.h"
 
-int (*get_func(char conv_spec))(va_list)
+/**
+*get_func - finds the correct function to run
+*@conv_spec: conversion specifier to match with correct function
+*Return: pointer to correct print function, NULL if no match found
+*/
+
+int (*get_func(char conv_spec))(va_list, int)
 {
-    int i = 0;
-    type_t get_type[] = {
-        {'c', print_char},
-        {'s', print_str},
-        {'d', print_dec},
-        {NULL, NULL}
+	int i = 0;
+	type_t get_type[] = {
+		{'c', print_char},
+		{'s', print_str},
+		{'d', print_dec},
+		{'i', print_dec},
+		{NULL, NULL}
 	};
 
-    for (i = 0; get_type[i].form_match != conv_spec; i++)
-    {
-        if (get_type[i].form_match == NULL)
-            return (NULL);
+	for (i = 0; get_type[i].form_match != conv_spec; i++)
+	{
+		if (get_type[i].form_match == NULL)
+			return (NULL);
 	}
-        return (get_type[i].func);
+		return (get_type[i].func);
 }
 
-int _printf (const char *format, ...)
+/**
+*_printf - prints out a line
+*@format: format to print out
+*Return: count of printed characters on success, a negative number on failure
+*/
+
+int _printf(const char *format, ...)
 {
 	va_list ap;
 	int i, count = 0;
-	int (*print)(va_list);
+	int (*print)(va_list, int);
 
 	va_start(ap, format);
 	if (format == NULL)
@@ -40,11 +53,13 @@ int _printf (const char *format, ...)
 				count += _putchar(format[i]);
 			}
 			else
-				count += print(ap);
+				count = print(ap, count);
 		}
 		else
 			count += _putchar(format[i]);
 		i++;
+	if (count == -1)
+		return (count);
 	}
 	return (count);
 }
